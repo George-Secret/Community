f56cfedb453328d1a1f0419e763356ec855f6531
George <georgexing1004@gmail.com>
2019-12-12 23:52:16 +0800

    删除idea的文件

--- src/main/java/cn/georgesecret/Service/ServiceImpl/UserServiceImpl.java
+++ src/main/java/cn/georgesecret/Service/ServiceImpl/UserServiceImpl.java
@@ -1,11 +1,9 @@
 package cn.georgesecret.Service.ServiceImpl;
 
 import cn.georgesecret.Service.UserService;
-import cn.georgesecret.entity.User;
 import cn.georgesecret.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.List;
 
 public class UserServiceImpl implements UserService {
     @Autowired
